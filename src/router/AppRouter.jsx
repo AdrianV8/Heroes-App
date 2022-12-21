8de68e62ebf6a9d13ef565574c5ad445acf4e7cd
Adrian <alumnoadrian9@gmail.com>
2022-12-21 10:13:32 +0100

    Creadas Rutas Públicas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,12 +3,18 @@ import { Navigate, Route, Routes } from "react-router-dom"
 import { LoginPage } from "../auth"
 import { HeroesRoutes} from "../heroes"
 import { PrivateRoute } from "./PrivateRoute"
+import { PublicRoute } from "./PublicRoute"
 
 export const AppRouter = () => {
   return (
     <>
         <Routes>
-            <Route path="login" element={<LoginPage/>} />
+
+            <Route path="login" element={
+              <PublicRoute>
+                <LoginPage/>
+              </PublicRoute>
+            } />
             
             {/* Protegemos la ruta principal de la aplicación en caso de no estar logueado */}
             <Route path="/*" element={
